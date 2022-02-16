27df4495e5f79d451b909593cea1e285da17e2b6
Romain Bessede <romain.bessede-ext@proapro.fr>
2022-02-16 09:28:02 +0100

    config.inc changement path

--- app/config/config.inc.php
+++ app/config/config.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 // Dossier racine
-define('PROJECT_ROOT', '/app');
+define('PROJECT_ROOT', '/app/app');
 
 // Chemin des vues twig
-define('BASE_PATH_TWIG', '/app/view');
+define('BASE_PATH_TWIG', '/app/app/view');
 
 // Activation ou désactivation des modules de l'application
 define('PROJECT_MODULES', array(
