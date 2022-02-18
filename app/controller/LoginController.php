ec198e069bcda0377f26a98d6105bb98a2fdd8f2
Romain Bessede <romain.bessede-ext@proapro.fr>
2022-02-18 15:17:49 +0100

    Page login

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace Pap\Gescom\Controller;
 
-use Pap\Gescom\tools\ConnectionTools;
+use Pap\Gescom\Tools\ConnectionTools;
 use Pap\Gescom\Model\manager\LoginManager;
 use Pap\Gescom\Model\manager\Entity\LoginEntity;
 
@@ -18,24 +18,13 @@ class LoginController
         $stat = pg_connection_status($connection);
 
         if ($stat === PGSQL_CONNECTION_OK) {
-
-            /*
-                        $objLoginEntity = new LoginEntity([
-                            'mail' => 'aurelien.boisselet@gmail.com',
-                        ]);
-                        $params = [
-                            "mail" => $objLoginEntity->getMail(),
-                        ];
-                        echo $params["mail"];
-            */
-
             $params = [
                 "user" => $_GET['user'],
                 "pwd" => sha1($_GET['pwd'])
             ];
 
-            $result = pg_prepare($connection, "my_query", 'SELECT * FROM users WHERE name = $1 AND mdp = $2');
-            $result = pg_execute($connection, "my_query", array($params["user"],$params["pwd"]));
+            $result = pg_prepare($connection, "queryUser", 'SELECT * FROM users WHERE name = $1 AND mdp = $2');
+            $result = pg_execute($connection, "queryUser", array($params["user"],$params["pwd"]));
 
             if (!$result) {
                 echo "Une erreur est survenue.\n";
