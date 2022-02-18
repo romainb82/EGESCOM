7e9089b49a85c7f702edd40ac3178586ca14d445
Romain Bessede <romain.bessede-ext@proapro.fr>
2022-02-18 15:29:42 +0100

    Page login

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -13,34 +13,21 @@ class LoginController
 
     public function index()
     {
-        $db = new ConnexionTools ;
-        $connection = $db->index() or die("Connexion impossible");
-        $stat = pg_connection_status($connection);
 
-        if ($stat === PGSQL_CONNECTION_OK) {
-            $params = [
-                "user" => $_GET['user'],
-                "pwd" => sha1($_GET['pwd'])
-            ];
+        $params = [
+            "user" => $_GET['user'],
+            "pwd" => sha1($_GET['pwd'])
+        ];
 
-            $result = pg_prepare($connection, "queryUser", 'SELECT * FROM users WHERE name = $1 AND mdp = $2');
-            $result = pg_execute($connection, "queryUser", array($params["user"],$params["pwd"]));
+        $model = new LoginManager();
+        $data = $model->select($params);
 
-            if (!$result) {
-                echo "Une erreur est survenue.\n";
-                exit;
-            }else{
-                $rows = pg_num_rows($result);
-                if($rows == 1){
-                    header('Location: /app/dashboard');
-                }else{
-                    header('Location: /');
-                }
-
-            }
-        } else {
-            echo 'Connexion erronée';
-            exit;
+        if($data["rows"] === 1){
+            $_SESSION['name'] = $data["name"];
+            $_SESSION['mail'] = $data["mail"];
+            header('Location: /app/dashboard');
+        }else{
+            header('Location: /app/?message='.$data["message"]);
         }
 
     }
--- app/model/manager/LoginManager.php
+++ app/model/manager/LoginManager.php
@@ -2,7 +2,49 @@
 
 namespace Pap\Gescom\Model\manager;
 
+use Pap\Gescom\Tools\ConnexionTools;
+
 class LoginManager
 {
+    public function select($params)
+    {
+        $arr = [];
+        $db = new ConnectionTools ;
+        $connection = $db->index();
+        $stat = pg_connection_status($connection);
+
+        if ($stat === PGSQL_CONNECTION_OK) {
+
+            /*
+                        $objLoginEntity = new LoginEntity([
+                            'mail' => 'aurelien.boisselet@gmail.com',
+                        ]);
+                        $params = [
+                            "mail" => $objLoginEntity->getMail(),
+                        ];
+                        echo $params["mail"];
+            */
+
+            $result = pg_prepare($connection, "my_query", 'SELECT * FROM users WHERE name = $1 AND pwd = $2');
+            $result = pg_execute($connection, "my_query", array($params["user"],$params["pwd"]));
+
+            if ($result) {
+                $arr['rows'] = pg_num_rows($result);
+                $arr['name'] = pg_fetch_result($result, 0, 'name');
+                $arr['mail'] = pg_fetch_result($result, 0, 'mail');
+            }else{
+                $arr['rows'] = 0;
+                $arr['message'] = 'Pas de résultat';
+            }
+
+        } else {
+            $arr['rows'] = 0;
+            $arr['message'] = 'Connexion impossible';
+        }
+
+        return $arr;
+
+
+    }
 
 }
